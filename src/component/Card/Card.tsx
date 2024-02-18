6ab83f18ad9493f389a852bd7576825ea98eb2f7
Brett Houtz <bcat38@gmail.com>
2024-02-18 12:24:23 -0500

    Progress on Card

--- src/component/Card/Card.tsx
+++ src/component/Card/Card.tsx
@@ -1,11 +1,10 @@
 import { Box, Group, MantineStyleProps, Stack } from '@mantine/core';
 import { PropsWithChildren } from 'react';
 import { Icon, Zap } from 'react-feather';
-
-const COLOR_CARD = '#e3ccaf';
+import classes from './Card.module.css';
 
 const Stat = ({ children, ...styleProps }: PropsWithChildren & MantineStyleProps) => (
-  <Box {...styleProps} style={{ lineHeight: 1, fontSize: 11, padding: 2, borderRadius: 4 }}>
+  <Box {...styleProps} className={classes.stat}>
     {children}
   </Box>
 );
@@ -17,29 +16,11 @@ interface EffectProps extends MantineStyleProps {
 }
 
 const Effect = ({ Icon, value, size, ...styleProps }: EffectProps) => (
-  <Box
-    c={'gray.7'}
-    h={size}
-    {...styleProps}
-    style={{ display: 'block', position: 'relative', lineHeight: 1 }}
-  >
+  <Box c={'gray.7'} h={size} {...styleProps} className={classes.effect}>
     <Icon size={size} />
     {typeof value === 'number' && (
-      <Stack
-        justify="center"
-        align="center"
-        style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
-      >
-        <Box
-          style={{
-            padding: '3px 4px',
-            fontSize: 11,
-            lineHeight: 1,
-            background: `radial-gradient(${COLOR_CARD}, ${COLOR_CARD}dd 40%, transparent)`,
-          }}
-        >
-          {value}
-        </Box>
+      <Stack justify="center" align="center" className={classes.effectValueOuter}>
+        <div className={classes.effectValueInner}>{value}</div>
       </Stack>
     )}
   </Box>
@@ -47,22 +28,32 @@ const Effect = ({ Icon, value, size, ...styleProps }: EffectProps) => (
 
 export const Card = () => {
   return (
-    <Stack bg={COLOR_CARD} w={55} h={77} p={2} gap={4}>
-      <Group gap={2} justify="space-between">
-        <Group gap={2} flex={1}>
-          <Stat bg={'black'}>5</Stat>
-          <Stat bg={'violet.9'}>2</Stat>
+    <div className={classes.card}>
+      <Stack gap={4} justify="space-between" h="100%">
+        <Group gap={2} justify="space-between">
+          <Group gap={2} flex={1}>
+            <Stat bg={'black'}>5</Stat>
+            <Stat bg={'violet.9'}>2</Stat>
+          </Group>
+          <Group gap={2} flex={1} justify="flex-end">
+            <Stat bg={'red.9'}>9</Stat>
+            <Stat bg={'blue.9'}>7</Stat>
+            {/* <Stat bg={'blue.9'}>7</Stat> */}
+          </Group>
         </Group>
-        <Group gap={2} flex={1} justify="flex-end">
-          <Stat bg={'red.9'}>9</Stat>
-          <Stat bg={'blue.9'}>7</Stat>
-        </Group>
-      </Group>
-      <Group gap={1} justify="space-between">
-        <Effect size={24} value={2} Icon={Zap} />
-        <Effect size={24} Icon={Zap} />
-        <Effect size={16} Icon={Zap} />
-      </Group>
-    </Stack>
+        <Stack gap={2} align="center" justify="space-around" className={classes.effectsSection}>
+          <Group gap={1} justify="space-around" w="100%">
+            <Effect size={24} value={2} Icon={Zap} />
+            <Effect size={24} Icon={Zap} />
+          </Group>
+          {/* <Group gap={1} w="100%" justify="space-around">
+            <Effect size={16} Icon={Zap} />
+          </Group> */}
+        </Stack>
+      </Stack>
+      <Stack align="center" className={classes.canvasOuter}>
+        <div className={classes.canvasInner}></div>
+      </Stack>
+    </div>
   );
 };

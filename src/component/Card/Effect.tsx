4e938144e1906be10f15db1c2872b5692bb504d3
Brett Houtz <bcat38@gmail.com>
2024-02-18 19:27:10 -0500

    Update attack icon

--- src/component/Card/Effect.tsx
+++ src/component/Card/Effect.tsx
@@ -1,10 +1,10 @@
 import { CardEffect, CardEffectType } from '@/card';
 import { MantineStyleProps, Box, Stack } from '@mantine/core';
-import { Icon, Zap } from 'react-feather';
+import PlainDagger from '@/asset/game-icons/plain-dagger.svg?react';
 import classes from './Card.module.css';
 
-const effectIcons: Record<CardEffectType, Icon> = {
-  [CardEffectType.ATTACK]: Zap,
+const effectIcons: Record<CardEffectType, React.FunctionComponent<React.ComponentProps<'svg'>>> = {
+  [CardEffectType.ATTACK]: PlainDagger,
 };
 
 export interface EffectProps extends MantineStyleProps, CardEffect {
@@ -15,7 +15,7 @@ export const Effect = ({ effectType, value, size, ...styleProps }: EffectProps) 
   const Icon = effectIcons[effectType];
   return (
     <Box c={'gray.7'} h={size} {...styleProps} className={classes.effect}>
-      <Icon size={size} />
+      <Icon width={size} height={size} />
       {typeof value === 'number' && (
         <Stack justify="center" align="center" className={classes.effectValueOuter}>
           <div className={classes.effectValueInner}>{value}</div>
--- src/component/Card/card-display.ts
+++ src/component/Card/card-display.ts
@@ -1,5 +1,5 @@
 import { CardId } from '@/card';
-import slimeGreen from '@/asset/slimeGreen.png';
+import slimeGreen from '@/asset/character/slimeGreen.png';
 
 export const cardImages: Record<CardId, string> = {
   [CardId.SLIME]: slimeGreen,
